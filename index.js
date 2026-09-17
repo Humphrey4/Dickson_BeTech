5c06a69b9a7642ac717b2d2388df17311037bc27
Humphrey Dickson <dicksonhumphrey4@gmail.com>
2026-09-17 03:40:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('hello, world!');
+const name = "Dickson";
+const age = 25;
+const favoriteProgrammingLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteProgrammingLanguage);
